ffa3e2b2275f8419ebf9ceda496ebb17aa592feb
christianodavid <christianodvids@icloud.com>
2024-04-23 12:49:00 -0300

    feat: implemented Texts Data and Text Card for Text Section

---
+++ src/components/pages/home/TextsSection/TextCard/TextCard.jsx
@@ -0,0 +1,21 @@
+import ProjectLink from "./ProjectLink";
+import styles from "./TextCard.module.css";
+
+const TextCard = ({ data }) => {
+  return (
+    <div className={styles.text_card}>
+      <img
+        className={styles.text_card__image}
+        src={data.backgroundImage0}
+        alt={data.alt}
+      />
+      <div className={styles.text_card__content}>
+        <h3 className={styles.text_card__content_title}>{data.title}</h3>
+        <p className={styles.text_card__content_paragraph}>{data.paragraph}</p>
+        <ProjectLink href={data.link}>Leia Aqui</ProjectLink>
+      </div>
+    </div>
+  );
+};
+
+export default TextCard;
---
+++ src/data/TextsData.jsx
@@ -0,0 +1,34 @@
+const TextsData = [
+  {
+    backgroundImage0: "./images/economy00.png",
+    alt: "Mulher abrindo a carteira",
+    title: "Economia",
+    paragraph: "Mais de 40% da população paranaense está inadimplente",
+    link: "https://medium.com/@gabrieladanieljo/mais-de-40-da-popula%C3%A7%C3%A3o-paranaense-est%C3%A1-inadimplente-98b52ad2cd25",
+  },
+  {
+    backgroundImage0: "./images/economy01.png",
+    alt: "Aviões no pátio do aeroporto",
+    title: "Economia",
+    paragraph: "Preço das passagens aéreas no Brasil sobe 13,47% em setembro",
+    link: "https://medium.com/@gabrieladanieljo/pre%C3%A7o-das-passagens-a%C3%A9reas-no-brasil-sobe-13-47-em-setembro-9f8e3362b814",
+  },
+  {
+    backgroundImage0: "./images/health00.png",
+    alt: "Poster da Campanha do Outubro Rosa",
+    title: "Saúde",
+    paragraph:
+      "INCA estima mais de 66 mil novos casos de câncer de mama no Brasil em 2022",
+    link: "https://medium.com/@gabrieladanieljo/inca-estima-mais-de-66-mil-novos-casos-de-câncer-de-mama-no-brasil-em-2022-3dddebf7cd46",
+  },
+  {
+    backgroundImage0: "./images/nature00.png",
+    alt: "Pessoas plantando árvores",
+    title: "Sustentabilidade",
+    paragraph:
+      "Estudantes da Universidade Tuiuti organizam mutirões para diminuir o desperdício de alimentos",
+    link: "https://medium.com/@gabrieladanieljo/estudantes-8f255587ee52",
+  },
+];
+
+export default TextsData;
