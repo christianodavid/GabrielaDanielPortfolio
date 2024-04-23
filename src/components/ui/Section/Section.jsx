c5b802fa3c064e7d1eb52bb070b2b3e2e2492e70
christianodavid <christianodvids@icloud.com>
2024-04-23 10:32:14 -0300

    feat: implemented custom Section and SectionTitle

---
+++ src/components/ui/Section/Section.jsx
@@ -0,0 +1,11 @@
+import styles from "./Section.module.css";
+
+const Section = ({ id, children }) => {
+  return (
+    <section id={id} className={styles.section}>
+      {children}
+    </section>
+  );
+};
+
+export default Section;
